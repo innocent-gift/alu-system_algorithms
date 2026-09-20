b63571627667dd6266f880a4f4ccef9254e86ce1
Nkurunziza Innocent <i.nkurunziz@alustudent.com>
2026-09-20 23:16:45 +0200

    done

--- graphs/graphs.h
+++ graphs/graphs.h
@@ -12,8 +12,8 @@
  */
 typedef enum edge_type_e
 {
-	UNIDIRECTIONAL = 0,
-	BIDIRECTIONAL
+UNIDIRECTIONAL = 0,
+BIDIRECTIONAL
 } edge_type_t;
 
 /* Define the structure temporarily for usage in the edge_t */
@@ -28,8 +28,8 @@ typedef struct vertex_s vertex_t;
  */
 typedef struct edge_s
 {
-	vertex_t	*dest;
-	struct edge_s	*next;
+vertex_t*dest;
+struct edge_s*next;
 } edge_t;
 
 /**
@@ -43,11 +43,11 @@ typedef struct edge_s
  */
 struct vertex_s
 {
-	size_t		index;
-	char		*content;
-	size_t		nb_edges;
-	edge_t		*edges;
-	struct vertex_s *next;
+size_tindex;
+char*content;
+size_tnb_edges;
+edge_t*edges;
+struct vertex_s *next;
 };
 
 /**
@@ -59,10 +59,19 @@ struct vertex_s
  */
 typedef struct graph_s
 {
-	size_t		nb_vertices;
-	vertex_t	*vertices;
+size_tnb_vertices;
+vertex_t*vertices;
 } graph_t;
 
+/* Function Prototypes */
 graph_t *graph_create(void);
+vertex_t *graph_add_vertex(graph_t *graph, const char *str);
+int graph_add_edge(graph_t *graph, const char *src, const char *dest,
+   edge_type_t type);
+void graph_delete(graph_t *graph);
+size_t depth_first_traverse(const graph_t *graph,
+    void (*action)(const vertex_t *v, size_t depth));
+size_t breadth_first_traverse(const graph_t *graph,
+    void (*action)(const vertex_t *v, size_t depth));
 
 #endif /* GRAPHS_H */
